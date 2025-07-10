cd17509ec42b56c59cb3709a59417034698dfa44
MAJ SPACE <majspaceofficial@gmail.com>
2025-07-10 13:36:33 +0800

    Add files via upload

--- utils.sh
+++ utils.sh
@@ -31,10 +31,10 @@ SECTION_DIVIDER="${GRAY}----------------------------------------${RESET}"
 
 # ASCII Art yang Diperbarui dengan Kesan Gradasi
 TITLE_ART="
-${DARK_BLUE}   ╔════╗ ╔════╗ ╔════╗ ╔════╗${RESET}
-${LIGHT_CYAN}   ║    ║ ║    ║ ║    ║ ║    ║${RESET}
-${DARK_BLUE}   ╚════╝ ╚════╝ ╚════╝ ╚════╝${RESET}
-${PURPLE}           MAJ SPACE SCRIPT${RESET}
+${DARK_BLUE}        ╔════╗ ╔════╗ ╔════╗ ╔════╗${RESET}
+${LIGHT_CYAN}        ║    ║ ║    ║ ║    ║ ║    ║${RESET}
+${DARK_BLUE}        ╚════╝ ╚════╝ ╚════╝ ╚════╝${RESET}
+${PURPLE}                MAJ SPACE SCRIPT${RESET}
 "
 
 # Papar tajuk dengan maklumat sistem
